cb2a45d56d2d5e03b1796c6ce9d128ea8ff108c5
Eduardo347387 <143133753+Eduardo347387@users.noreply.github.com>
2024-02-14 15:30:09 -0600

    update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./_styles.scss']
+  styleUrls: []
 })
 export class AppComponent {
   title = 'portfolio';
